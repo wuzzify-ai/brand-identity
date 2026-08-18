6a044c2fa95b1bda7f62af966b51e8dc79102e4e
a.saad <a.saad@alrajhitakaful.com>
2026-08-18 16:24:08 +0300

    enable cors

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -11,8 +11,7 @@ async function bootstrap() {
 
   app.setGlobalPrefix('v1');
   app.enableCors({
-    origin: process.env.WEB_ORIGIN?.split(',').map((origin) => origin.trim()).filter(Boolean) ?? ['http://localhost:3000'],
-    credentials: true
+    origin: '*',
   });
   app.enableShutdownHooks();
   app.useGlobalPipes(
